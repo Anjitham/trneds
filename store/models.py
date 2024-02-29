78f1d96bbd26795c5a4fb110312179eb8d407bc2
Anjitham <Anjithamanazhi520@gmail.com>
2024-02-29 22:26:57 +0530

    Fourth commit

--- store/models.py
+++ store/models.py
@@ -44,6 +44,22 @@ class Basket(models.Model):
     updated_date=models.DateTimeField(auto_now=True)
     is_active=models.BooleanField(default=True)
 
+    @property
+    def cart_item(self):
+        return self.cartitem.filter(is_order_placed=False)
+    
+    @property
+    def cart_item_count(self):
+        return self.cart_item.count()
+    
+    @property
+    def basket_total(self):
+        basket_item=self.cart_item
+        if basket_item:
+            total=sum([bi.item_total for bi in basket_item])
+            return total
+        return 0
+
 
 class BasketItem(models.Model):
     product_object=models.ForeignKey(Product,on_delete=models.CASCADE)
@@ -53,7 +69,11 @@ class BasketItem(models.Model):
     created_date=models.DateTimeField(auto_now_add=True)
     updated_date=models.DateTimeField(auto_now=True)
     is_active=models.BooleanField(default=True)
-    is_order_placed=models.BooleanField(default=False,)
+    is_order_placed=models.BooleanField(default=False)
+
+    @property
+    def item_total(self):
+        return self.qty*self.product_object.price
 
 # default basket creation
 def create_basket(sender,instance,created,**kwargs):
